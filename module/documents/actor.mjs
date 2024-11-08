95d820eb44c89050cc90af3bb3dfbd960a513184
patrickmohrmann <patrick.mohrmann@outlook.de>
2024-11-08 16:19:27 +0100

    reworked comments
    rework

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -61,7 +61,8 @@ export default class ActorEd extends Actor {
    * @type {Item[]}
    */
   get disciplines() {
-    return this.items.filter( item => item.type === "discipline" );
+    // return this.items.filter( item => item.type === "discipline" );
+    return this.itemTypes.discipline;
   }
 
   /**
