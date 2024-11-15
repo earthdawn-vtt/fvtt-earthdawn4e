f673012f2123d80d160786f743779032b5121190
Chris <christopherDick00@gmail.com>
2024-11-15 20:39:59 +0100

    Add basic spell data model

--- module/data/item/spell.mjs
+++ module/data/item/spell.mjs
@@ -3,6 +3,9 @@ import LearnableTemplate from "./templates/learnable.mjs";
 import ED4E from "../../config.mjs";
 import LearnSpellPrompt from "../../applications/advancement/learn-spell.mjs";
 import { ItemDataModel } from "../abstract.mjs";
+import TargetTemplate from "./templates/targeting.mjs";
+
+
 
 /**
  * Data model template with information on Spell items.
@@ -10,15 +13,239 @@ import { ItemDataModel } from "../abstract.mjs";
  */
 export default class SpellData extends ItemDataModel.mixin(
   ItemDescriptionTemplate,
-  LearnableTemplate
+  LearnableTemplate,
+  TargetTemplate
 )  {
 
   /** @inheritDoc */
   static defineSchema() {
+    const { StringField, NumberField, SchemaField, SetField } = foundry.data.fields;
+
     return this.mergeSchema( super.defineSchema(), {
-            
+      spellcastingType: new StringField( {
+        required: true,
+        nullable: true,
+        blank:    false,
+        trim:     true,
+        choices:  ED4E.spellcastingTypes,
+        label:    this.labelKey( "spellcastingType" ),
+        hint:     this.hintKey( "spellcastingType" ),
+      } ),
+      level: new NumberField( {
+        required: true,
+        nullable: false,
+        min:      1,
+        initial:  1,
+        integer:  true,
+        positive: true,
+        label:    this.labelKey( "level" ),
+        hint:     this.hintKey( "level" ),
+      } ),
+      spellDifficulty:    new SchemaField( {
+        reattune: new NumberField( {
+          required: true,
+          nullable: false,
+          min:      ED4E.minDifficulty,
+          initial:  ( data ) => { return data.weaving + 5 || ED4E.minDifficulty; },
+          integer:  true,
+          label:    this.labelKey( "reattuneDifficulty" ),
+          hint:     this.hintKey( "reattuneDifficulty" ),
+        } ),
+        weaving: new NumberField( {
+          required: true,
+          nullable: false,
+          min:      ED4E.minDifficulty,
+          initial:  ( _ ) => { return this.parent?.parent?.fields?.level?.initial + 4 || ED4E.minDifficulty; },
+          integer:  true,
+          label:    this.labelKey( "weavingDifficulty" ),
+          hint:     this.hintKey( "weavingDifficulty" ),
+        } ),
+      } ),
+      threads: new SchemaField( {
+        required: new NumberField( {
+          required: true,
+          nullable: false,
+          min:      0,
+          initial:  0,
+          integer:  true,
+          label:    this.labelKey( "spellThreadsRequired" ),
+          hint:     this.hintKey( "spellThreadsRequired" ),
+        } ),
+        woven: new NumberField( {
+          required: true,
+          nullable: false,
+          min:      0,
+          initial:  0,
+          integer:  true,
+          label:    this.labelKey( "spellThreadsWoven" ),
+          hint:     this.hintKey( "spellThreadsWoven" ),
+        } ),
+        extra: new NumberField( {} ),
+      } ),
+      effect: new StringField( {
+        required: true,
+        blank:    true,
+        initial:  "",
+        label:    "ED.Item.Spell.effect"
+      } ),
+      keywords: new SetField( new StringField( {
+        required: true,
+        nullable: false,
+        blank:    false,
+        trim:     true,
+        choices:  ED4E.spellKeywords,
+        label:    "ED.Item.Spell.keywords"
+      } ), {
+        required: true,
+        nullable: false,
+        initial:  [],
+      } ),
+      element: new SchemaField( {
+        type: new StringField( {
+          required: true,
+          nullable: true,
+          blank:    false,
+          trim:     true,
+          choices:  ED4E.elements,
+          label:    this.labelKey( "spellElementType" ),
+          hint:     this.hintKey( "spellElementType" ),
+        } ),
+        subtype: new StringField( {
+          required: true,
+          nullable: true,
+          blank:    false,
+          trim:     true,
+          choices:  Object.values(
+            ED4E.elementSubtypes
+          ).map(
+            subtypes => Object.keys( subtypes )
+          ).flat(),
+          label:    this.labelKey( "spellElementSubtype" ),
+          hint:     this.hintKey( "spellElementSubtype" ),
+        } )
+      },
+      {
+        required: true,
+        nullable: true,
+        label:    this.labelKey( "spellElement" ),
+        hint:     this.hintKey( "spellElement" ),
+      } ),
+
+
+      duration: new SchemaField( {
+        value: new StringField( {
+          required: true,
+          blank:    false,
+          initial:  "0",
+          label:    "ED.Item.Spell.value"
+        } ),
+        uom: new StringField( {
+          required: true,
+          blank:    false,
+          initial:  "yard",
+          label:    "ED.Item.Spell.uom"
+        } )
+      },
+      {
+        label: "ED.Item.Spell.duration"
+      } ),
+      range: new SchemaField( {
+        value: new StringField( {
+          required: true,
+          blank:    false,
+          initial:  "0",
+          label:    "ED.Item.Spell.shape"
+        } ),
+        uom: new StringField( {
+          required: true,
+          blank:    false,
+          initial:  "yard",
+          label:    "ED.Item.Spell.uom"
+        } )
+      },
+      {
+        label: "ED.Item.Spell.range"
+      } ),
+      spellArea: new SchemaField( {
+        shape: new StringField( {
+          required: true,
+          blank:    false,
+          initial:  "0",
+          label:    "ED.Item.Spell.shape"
+        } ),
+        value: new StringField( {
+          required: true,
+          blank:    false,
+          initial:  "0",
+          label:    "ED.Item.Spell.shape"
+        } ),
+        uom: new StringField( {
+          required: true,
+          blank:    false,
+          initial:  "yard",
+          label:    "ED.Item.Spell.uom"
+        } )
+      },
+      {
+        label: "ED.Item.Spell.spellArea"
+      } ),
+
+      // extraSuccessesList: [],
+      // extraThreadsList: [],
     } );
   }
+
+
+  /**
+   * @inheritDoc
+   */
+  static _validateJoint( value ) {
+    if ( value.element.type ) {
+      const elemType = value.element.type;
+      const elemSubtype = value.element.subtype;
+
+      // subtype is optional
+      if ( !elemSubtype ) return undefined;
+
+      if ( !Object.keys( ED4E.elementSubtypes[ elemType ] ).includes( elemSubtype ) )
+        throw new Error( game.i18n.format( "ED.Errors.InvalidElementSubtype" ) );
+    }
+
+    // continue validation
+    return undefined;
+  }
+
+  /* -------------------------------------------- */
+  /*  Properties                                  */
+  /* -------------------------------------------- */
+
+  /**
+   * @description Whether this spell is an illusion and therefore can be sensed.
+   * @type {boolean}
+   */
+  get isIllusion() {
+    return this.keywords.has( "illusion" );
+  }
+
+  /**
+   * @description The difficulty number to dispel this spell.
+   * @type {number}
+   */
+  get dispelDifficulty() {
+    return this.level + 10;
+  }
+
+  /**
+   * @description The difficulty number to sense this spell, if it is an illusion, else undefined.
+   * @type { number | undefined }
+   */
+  get sensingDifficulty() {
+    return this.isIllusion ? this.level + 15 : undefined;
+  }
+
+  /* -------------------------------------------- */
+  /*  LP Tracking                                 */
+  /* -------------------------------------------- */
 
   /** @inheritDoc */
   get canBeLearned() {
@@ -44,7 +271,7 @@ export default class SpellData extends ItemDataModel.mixin(
   }
 
   /** @inheritDoc */
-  static async learn( actor, item, createData ) {
+  static async learn( actor, item, _ ) {
     if ( !item.system.canBeLearned ) {
       ui.notifications.warn( game.i18n.localize( "ED.Notifications.Warn.cannotLearn" ) );
       return;
@@ -91,4 +318,5 @@ export default class SpellData extends ItemDataModel.mixin(
     super.migrateData( source );
     // specific migration functions
   }
+
 }
