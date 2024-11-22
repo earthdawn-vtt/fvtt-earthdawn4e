614bc0412f8982b2bfe095a5af11b24a451d4402
Chris <christopherDick00@gmail.com>
2024-11-22 14:28:00 +0100

    Make RangeField labeling consistent with UnitField approach

--- module/data/fields/range-field.mjs
+++ module/data/fields/range-field.mjs
@@ -12,22 +12,42 @@ export default class RangeField extends UnitField {
   constructor( fields={}, options={} ) {
     super( fields, options );
 
-    this.fields.value.label = "ED.Data.Fields.Labels.Range.value";
-    this.fields.value.hint = "ED.Data.Fields.Hints.Range.value";
+    this.fields.value.label = `${this.labelKey}.value`;
+    this.fields.value.hint = `${this.hintKey}.value`;
 
-    this.fields.unit.label = "ED.Data.Fields.Labels.Range.unit";
-    this.fields.unit.hint = "ED.Data.Fields.Hints.Range.unit";
+    this.fields.unit.label = `${this.labelKey}.unit`;
+    this.fields.unit.hint = `${this.hintKey}.unit`;
     this.fields.unit.choices = ED4E.distanceUnits;
     this.fields.unit.initial = "any";
 
-    this.fields.special.label = "ED.Data.Fields.Labels.Range.special";
-    this.fields.special.hint = "ED.Data.Fields.Hints.Range.special";
+    this.fields.special.label = `${this.labelKey}.special`;
+    this.fields.special.hint = `${this.hintKey}.special`;
 
     this.scalarConfig = ED4E.movementUnits;
     this.unitGroupOptions = {
       "":                                                   ED4E.rangeTypes,
       "ED.Data.Fields.Options.Range.groupMovementUnits":    ED4E.movementUnits,
     };
+  }
+
+  /* -------------------------------------------- */
+  /*  Properties                                  */
+  /* -------------------------------------------- */
+
+  /**
+   * The default lang key for the object containing labels for the range field.
+   * @type {string}
+   */
+  get labelKey() {
+    return "ED.Data.Fields.Labels.Range";
+  }
+
+  /**
+   * The default lang key for the object containing hints for the range field.
+   * @type {string}
+   */
+  get hintKey() {
+    return "ED.Data.Fields.Hints.Range";
   }
 
 }
