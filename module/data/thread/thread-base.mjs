ead2f72b02b0594545db5f4f637fc7b79d7d71bc
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-01-01 23:45:46 +0100

    Added Thread Base Data Model information

--- module/data/thread/thread-base.mjs
+++ module/data/thread/thread-base.mjs
@@ -1,3 +1,4 @@
+import ED4E from "../../config.mjs";
 import { SparseDataModel } from "../abstract.mjs";
 import ThreadLevelData from "./thread-level.mjs";
 
@@ -6,14 +7,90 @@ export default class ThreadBaseData extends SparseDataModel {
   static defineSchema() {
     const fields = foundry.data.fields;
     return {
-      levels: new fields.ArrayField(
+      isIdentified: new fields.BooleanField( {
+        required: true,
+        initial:  false,
+        // label:    this.labelKey( "PhysicalItems.ThreadItem.isIdentified" ),
+        // hint:     this.hintKey( "PhysicalItems.ThreadItem.isIdentified" )
+        label:    "ED.Data.Item.Labels.PhysicalItems.ThreadItem.isIdentified",
+        hint:     "ED.Data.Item.Hints.PhysicalItems.ThreadItem.isIdentified"
+      } ),
+      isAnalysed: new fields.BooleanField( {
+        required: true,
+        initial:  false,
+        // label:    this.labelKey( "PhysicalItems.ThreadItem.isAnalysed" ),
+        // hint:     this.hintKey( "PhysicalItems.ThreadItem.isAnalysed" )
+        label:    "ED.Data.Item.Labels.PhysicalItems.ThreadItem.isAnalysed",
+        hint:     "ED.Data.Item.Hints.PhysicalItems.ThreadItem.isAnalysed"
+      } ),
+      // to be aligned with the path of actor mystical defense
+      characteristics: new fields.SchemaField( {
+        defenses: new fields.SchemaField( {
+          mystical: new fields.SchemaField( { 
+            baseValue: new fields.NumberField( {
+              required: true,
+              nullable: false,
+              min:      0,
+              step:     1,
+              initial:  0,
+              integer:  true,
+              // label:    this.labelKey( "PhysicalItems.ThreadItem.mysticalDefense" ),
+              // hint:     this.hintKey( "PhysicalItems.ThreadItem.mysticalDefense" )
+              label:    "ED.Data.Item.Labels.PhysicalItems.ThreadItem.mysticalDefense",
+              hint:     "ED.Data.Item.Hints.PhysicalItems.ThreadItem.mysticalDefense"
+            } ),
+            value: new fields.NumberField( {
+              required: true,
+              nullable: false,
+              min:      0,
+              step:     1,
+              initial:  0,
+              integer:  true,
+            } ),
+          } ),
+        } ),
+      } ),
+      maxThreads:         new fields.NumberField( { 
+        required: true,
+        nullable: false,
+        min:      1,
+        step:     1,
+        initial:  1,
+        integer:  true,
+        // label:    this.labelKey( "PhysicalItems.ThreadItem.maxThreads" ),
+        // hint:     this.hintKey( "PhysicalItems.ThreadItem.maxThreads" )
+        label:    "ED.Data.Item.Labels.PhysicalItems.ThreadItem.maxThreads",
+        hint:     "ED.Data.Item.Hints.PhysicalItems.ThreadItem.maxThreads"
+      } ),
+      tier:               new fields.StringField( { 
+        required: true,
+        nullable: false,
+        initial:  "novice",
+        choices:  ED4E.tier,
+        // label:    this.labelKey( "PhysicalItems.ThreadItem.tier" ),
+        // hint:     this.hintKey( "PhysicalItems.ThreadItem.tier" )
+        label:    "ED.Data.Item.Labels.PhysicalItems.ThreadItem.tier",
+        hint:     "ED.Data.Item.Hints.PhysicalItems.ThreadItem.tier"
+      } ),
+      enchantmentPattern: new fields.DocumentUUIDField( {
+        required: true,
+        nullable: true,
+        initial:  null,
+        // label:    this.labelKey( "PhysicalItems.ThreadItem.enchantmentPattern" ),
+        // hint:     this.hintKey( "PhysicalItems.ThreadItem.enchantmentPattern" )
+        label:    "ED.Data.Item.Labels.PhysicalItems.ThreadItem.enchantmentPattern",
+        hint:     "ED.Data.Item.Hints.PhysicalItems.ThreadItem.enchantmentPattern"
+      } ),
+      levels:     new fields.ArrayField(
         new fields.EmbeddedDataField(
           ThreadLevelData,
           {
             required: false,
             nullable: true,
-            label:    "ED.threadLevel",
-            hint:     "ED.threadLevel"
+            // label:    this.labelKey( "PhysicalItems.ThreadItem.threadlevel" ),
+            // hint:     this.hintKey( "PhysicalItems.ThreadItem.threadlevel" )
+            label:    "ED.Data.Item.Labels.PhysicalItems.ThreadItem.threadlevel",
+            hint:     "ED.Data.Item.Hints.PhysicalItems.ThreadItem.threadlevel"
           }
         ),
         {
@@ -21,6 +98,7 @@ export default class ThreadBaseData extends SparseDataModel {
           nullable: true,
           initial:  [],
         } ),
+        
     };
   }
 
