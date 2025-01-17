e89e2d2b1c0a3ea4a006b120f55a41355c7052b0
Chris <christopherDick00@gmail.com>
2025-01-17 16:05:53 +0100

    Add unit data models to module

--- module/data/common/_module.mjs
+++ module/data/common/_module.mjs
@@ -1,5 +1,11 @@
+import AreaUnitData from "./area-unit.mjs";
 import BaseUnitData from "./base-unit.mjs";
+import DurationUnitData from "./duration-unit.mjs";
+import RangeUnitData from "./range-unit.mjs";
 
 export {
+  AreaUnitData,
   BaseUnitData,
+  DurationUnitData,
+  RangeUnitData
 };
