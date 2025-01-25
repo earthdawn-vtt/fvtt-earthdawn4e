652eac5e46c457a398b4352af88c828e81b897ae
Chris <christopherDick00@gmail.com>
2025-01-25 14:30:42 +0100

    Add earthdawn active effect document type

--- module/documents/_module.mjs
+++ module/documents/_module.mjs
@@ -1,5 +1,6 @@
 // document classes
 export {default as ActorEd} from "./actor.mjs";
 export {default as ChatMessageEd} from "./chat.mjs";
+export {default as EarthdawnActiveEffect} from "./active-effect.mjs";
 export {default as ItemEd} from "./item.mjs";
 export {default as JournalEntryEd} from "./journal.mjs";
