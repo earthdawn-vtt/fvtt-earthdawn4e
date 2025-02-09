ca0562c198b9986d0ba7e17ce72086eab3c7fb3e
Chris <christopherDick00@gmail.com>
2025-02-09 17:20:59 +0100

    Remove unused unit fields
    Solved with data models

--- module/data/fields/_module.mjs
+++ module/data/fields/_module.mjs
@@ -3,13 +3,8 @@
 * https://gitlab.com/peginc/swade} systems.
 */
 
-export { default as AreaField } from "./area-field.mjs";
 export { default as DatetimeField } from "./datetime-field.mjs";
-export { default as DurationField } from "./duration-field.mjs";
 export { default as EdidField } from "./edid-field.mjs";
 export { default as FormulaField } from "./formula-field.mjs";
 export { default as IdentifierField } from "./identifier-field.mjs";
 export { default as MappingField } from "./mapping-field.mjs";
-export { default as RangeField } from "./range-field.mjs";
-export { default as SpellEnhancementField } from "./spell-enhancement-field.mjs";
-export { default as UnitField } from "./unit-field.mjs";
