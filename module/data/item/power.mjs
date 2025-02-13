7b02e7c8ea340ca36e805e11e80c050707d415f0
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-02-13 23:51:39 +0100

    add missing data fields for powers and attacks

--- module/data/item/power.mjs
+++ module/data/item/power.mjs
@@ -1,16 +1,35 @@
 import NoneNamegiverPowerData from "./templates/none-namegiver-power.mjs";
 import ItemDescriptionTemplate from "./templates/item-description.mjs";
+import ED4E from "../../config.mjs";
 
 /**
  * Data model template with information on Power items.
  */
 export default class PowerData extends NoneNamegiverPowerData.mixin(
-  ItemDescriptionTemplate
+  ItemDescriptionTemplate,
 )  {
 
   /** @inheritDoc */
   static defineSchema() {
+    const fields = foundry.data.fields;
     return this.mergeSchema( super.defineSchema(), {
+      armorType: new fields.StringField( {
+        required: true,
+        nullable: true,
+        blank:    true,
+        initial:  "",
+        choices:  ED4E.armor,
+        label:    this.labelKey( "Weapons.armorType" ),
+        hint:     this.hintKey( "Weapons.armorType" )
+      } ),
+      damage:        new fields.SchemaField( {
+        type: new fields.StringField( {
+          initial:  "standard",
+          choices:  ED4E.damageType,
+          label:    this.labelKey( "Weapons.damageType" ),
+          hint:     this.hintKey( "Weapons.damageType" )
+        } ),
+      } ),
     } );
   }
 
