8797a5f5a168d412ae5c3ac21cab864234f171d1
Chris <christopherDick00@gmail.com>
2025-02-27 18:34:17 +0100

    Change unconscious status icon

--- module/config/statuses.mjs
+++ module/config/statuses.mjs
@@ -1,8 +1,10 @@
+import { preLocalize } from "../utils.mjs";
+
 export const statusEffects = [
   {
     id:    "aggressive",
     label: "ED.ActiveEffect.Status.aggressive",
-    icon:  "systems/ed4e/icons/enraged.svg",
+    icon:  "systems/ed4e/assets/icons/enraged.svg",
   },
   {
     id:    "blindness",
@@ -12,22 +14,22 @@ export const statusEffects = [
   {
     id:    "blindsided",
     label: "ED.ActiveEffect.Status.blindsided",
-    icon:  "systems/ed4e/icons/backstab.svg",
+    icon:  "systems/ed4e/assets/icons/backstab.svg",
   },
   {
     id:    "cover",
     label: "ED.ActiveEffect.Status.cover",
-    icon:  "systems/ed4e/icons/broken-wall.svg",
+    icon:  "systems/ed4e/assets/icons/broken-wall.svg",
   },
   {
     id:    "darkness",
     label: "ED.ActiveEffect.Status.darkness",
-    icon:  "systems/ed4e/icons/fog.svg",
+    icon:  "systems/ed4e/assets/icons/fog.svg",
   },
   {
     id:    "dazzled",
     label: "ED.ActiveEffect.Status.dazzled",
-    icon:  "systems/ed4e/icons/laser-sparks.svg",
+    icon:  "systems/ed4e/assets/icons/laser-sparks.svg",
   },
   {
     id:    "dead",
@@ -37,22 +39,22 @@ export const statusEffects = [
   {
     id:    "defensive",
     label: "ED.ActiveEffect.Status.defensive",
-    icon:  "systems/ed4e/icons/surrounded-shield.svg",
+    icon:  "systems/ed4e/assets/icons/surrounded-shield.svg",
   },
   {
     id:    "grappled",
     label: "ED.ActiveEffect.Status.grappled",
-    icon:  "systems/ed4e/icons/grab.svg",
+    icon:  "systems/ed4e/assets/icons/grab.svg",
   },
   {
     id:    "harried",
     label: "ED.ActiveEffect.Status.harried",
-    icon:  "systems/ed4e/icons/meeple-army.svg",
+    icon:  "systems/ed4e/assets/icons/meeple-army.svg",
   },
   {
     id:    "impaired",
     label: "ED.ActiveEffect.Status.impaired",
-    icon:  "systems/ed4e/icons/achilles-heel.svg",
+    icon:  "systems/ed4e/assets/icons/achilles-heel.svg",
   },
   {
     id:    "knockedDown",
@@ -62,19 +64,20 @@ export const statusEffects = [
   {
     id:    "overwhelmed",
     label: "ED.ActiveEffect.Status.overwhelmed",
-    icon:  "systems/ed4e/icons/dozen.svg",
+    icon:  "systems/ed4e/assets/icons/dozen.svg",
   },
   {
     id:    "surprised",
     label: "ED.ActiveEffect.Status.surprised",
-    icon:  "systems/ed4e/icons/surprised.svg",
+    icon:  "systems/ed4e/assets/icons/surprised.svg",
   },
   {
     id:    "unconscious",
     label: "ED.ActiveEffect.Status.unconscious",
-    icon:  "icons/svg/unconscious.svg",
+    icon:  "systems/ed4e/assets/icons/dead-head.svg",
   },
 ];
+preLocalize( "statusEffects", { key: "label" } );
 
 export const specialStatusEffects = {
   BLIND:     "blindness",
