d5d4b9492605feabc60aa8194cfc5c8856fd7281
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-03-13 17:01:05 +0100

    set discipline level to 1 after creation

--- module/data/actor/pc.mjs
+++ module/data/actor/pc.mjs
@@ -170,6 +170,9 @@ export default class PcData extends NamegiverTemplate {
           }
         } );
       }
+      await disciplineAfterCreation.update( {
+        "system.level": 1
+      } );
     }
 
     const actorApp = newActor.sheet.render( true, {focus: true} );
