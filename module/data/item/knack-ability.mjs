f50bce7ba076e50874bfc119b7fd22e34853597e
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-03-22 23:46:23 +0100

    migrate knack abilities

--- module/data/item/knack-ability.mjs
+++ module/data/item/knack-ability.mjs
@@ -66,8 +66,54 @@ export default class KnackAbilityData extends AbilityTemplate.mixin(
   /* -------------------------------------------- */
 
   /** @inheritDoc */
+  // eslint-disable-next-line complexity
   static migrateData( source ) {
-    super.migrateData( source );
-    // specific migration functions
+    const oldTargetDefense = [ "", "physicaldefense", "mysticaldefense", "socialdefense" ];
+    const newTargetDefense = [ "", "physical", "mystical", "social" ];
+    const oldGroupDifficulty = [ "", "defenseLow", "defenseLowPlus", "defenseHigh", "defenseHighPlus" ];
+    const newGroupDifficulty = [ "", "lowestOfGroup", "lowestX", "highestOfGroup", "highestX" ];
+    const oldAttributes = [ "", "dexterityStep", "strengthStep", "toughnessStep", "perceptionStep", "willpowerStep", "charismaStep", "initiativeStep" ];
+    const newAttributes = [ "", "dex", "str", "tou", "per", "wil", "cha", "" ];
+    const oldActions = [ "", "Standard", "Simple", "Free", "Sustained" ];
+    const newActions = [ "", "standard", "simple", "free", "sustained" ];
+    
+    // Migrate action (ok)
+    if ( oldActions.includes( source.action ) ) {
+      source.action = newActions[oldActions.indexOf( source.action )];
+    }
+  
+    // Migrate defense target (ok)
+    if ( oldTargetDefense.includes( source.defenseTarget ) || oldGroupDifficulty.includes( source.defenseGroup ) ) {
+      source.difficulty = {
+        target: newTargetDefense[oldTargetDefense.indexOf( source.defenseTarget )],
+        group:  newGroupDifficulty[oldGroupDifficulty.indexOf( source.defenseGroup )]
+      };
+    }
+
+    // set rollType for recovery (ok)
+    if ( source.healing > 0 ) {
+      if ( source.rollType === undefined ) {
+        source.rollType = "recovery";
+      }
+    } 
+  
+    // Migrate attribute (ok)
+    if ( oldAttributes.includes( source.attribute ) ) {
+      if ( source.attribute === "initiativeStep" ) {
+        source.rollType = "initiative";
+      } 
+      source.attribute = newAttributes[oldAttributes.indexOf( source.attribute )];
+    }
+
+    // Migrate description
+    if ( typeof source.description === "string" ) {
+      source.description = { value: source.description };
+    }
+
+    // Migrate restrictions
+    // restrictions and requirements are currently not done
+    source.restrictions = [];
+    source.requirements = [];
+    
   }
 }
