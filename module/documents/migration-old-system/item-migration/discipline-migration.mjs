e438daf7d57081df3c28d0f1b18f8852775690df
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-03-22 23:27:42 +0100

    create migration file

---
+++ module/documents/migration-old-system/item-migration/discipline-migration.mjs
@@ -0,0 +1,7 @@
+export default class DisciplineMigration {
+    
+  static async migrateData( source ) {
+
+    return source; // Return the modified data
+  }
+}
---
+++ module/documents/migration-old-system/item-migration/skill-migration.mjs
@@ -0,0 +1,7 @@
+export default class SkillMigration {
+    
+  static async migrateData( source ) {
+
+    return source; // Return the modified data
+  }
+}
