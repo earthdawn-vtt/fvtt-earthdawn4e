6c7f7911636cb75b092d8780ae4f32ba1140a554
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-03-22 23:23:48 +0100

    create const for migration

---
+++ module/documents/migration-old-system/migration-partials/migration-const.mjs
@@ -0,0 +1,77 @@
+// Spellcasting relevant
+export const oldThreadWeavingTalentNames = [
+  "Fadenweben (Beschützerweben)",
+  "Fadenweben (Diebweben)",
+  "Fadenweben (Elementarismus)",
+  "Fadenweben (Fadenschmieden)",
+  "Fadenweben (Flussweben)",
+  "Fadenweben (Geisterbeschwörung)",
+  "Fadenweben (Geschichtenweben)",
+  "Fadenweben (Himmelsweben)",
+  "Fadenweben (Illusionismus)",
+  "Fadenweben (Kriegsweben)",
+  "Fadenweben (Kundschafterweben)",
+  "Fadenweben (Luftweben)",
+  "Fadenweben (Magie)",
+  "Fadenweben (Meeresweben)",
+  "Fadenweben (Pfeilweben)",
+  "Fadenweben (Reiterweben)",
+  "Fadenweben (Schamanismus)",
+  "Fadenweben (Tierweben)",
+  "Fadenweben (Waffenweben)",
+  "Thread Weaving (ENGLISH)",
+];
+  
+export const oldThreadWeavingTalentNameForCasters = {
+  elementalism: "Fadenweben (Elementarismus)",
+  illusionism:  "Fadenweben (Illusionismus)",
+  nethermancy:  "Fadenweben (Geisterbeschwörung)",
+  shamanism:    "Fadenweben (Schamanismus)",
+  wizardry:     "Fadenweben (Magie)",
+};
+  
+export const oldSpellcastingTalentNames = [ "Spellcasting", "Spruchzauberei" ];
+export const patterncraftNames = [ "Patterncraft", "Struktur Verstehen" ];
+export const versatilityTalentNames = [ "Versatility", "Vielseitigkeit" ];
+  
+// Damage adder
+export const oldDamageAdderNames = [
+  "Schmetterschlag",
+  "Brandpfeil",
+  "Sturmangriff",
+  "Hammerschlag",
+  "Krallenhand",
+  "Körperkontrolle",
+  "Überraschungsschlag",
+  "Willensstärke",
+];
+  
+// Combat talents
+export const unarmedCombatNames = [ "Unarmed Combat", "Waffenloser Kampf" ];
+export const meleeWeaponNames = [ "Melee Weapons", "Nahkampfwaffen" ];
+export const missileWeaponNames = [ "Missile Weapons", "Projektilwaffen" ];
+export const throwingWeaponNames = [ "Throwing Weapons", "Wurfwaffen" ];
+  
+// Reactions
+export const physicalReactionNames = [ "Avoid Blow", "Riposte", "Hieb Ausweichen" ];
+export const mysticReactionNames = [ "Steel Thought", "Eiserner Wille" ];
+export const socialReactionNames = [ "Resist Taunt", "Starrsinn" ];
+  
+// Required item status of used weapon
+export const itemStatusMeleeWeapons = [ "mainHand", "twoHands" ];
+export const itemStatusMissileWeapons = [ "twoHands" ];
+export const itemStatusThrowingWeapons = [ "mainHand" ];
+
+export const oldActions = [ "", "Standard", "Simple", "Free", "Sustained" ];
+export const newActions = [ "", "standard", "simple", "free", "sustained" ];
+
+// Difficulty settings
+export const oldTargetDefense = [ "", "physicaldefense", "mysticaldefense", "socialdefense" ];
+export const newTargetDefense = [ "", "physical", "mystical", "social" ];
+export const oldGroupDifficulty = [ "", "defenseLow", "defenseLowPlus", "defenseHigh", "defenseHighPlus" ];
+export const newGroupDifficulty = [ "", "lowestOfGroup", "lowestX", "highestOfGroup", "highestX" ];
+
+// Equipment specific names
+export const ammunitionArrowNames = [ "Windlingspfeile", "Langbogenpfeile", "Kurzbogenpfeile", "Kriegsbogenpfeile" ];
+export const ammunitionBoltNames = [ "Mittlere Bolzen", "Leichte Bolzen" ];
+export const ammunitionNeedleNames = [ "Schachtel mit 10 Nadeln" ];
