775fd69bf6177416083f0f4f241110ab5d662f65
Chris S. D. <christopherDick00@gmail.com>
2025-03-29 15:39:32 +0100

    Add function to create unique static status IDs

--- module/utils.mjs
+++ module/utils.mjs
@@ -261,6 +261,16 @@ export function replaceFormulaData( formula, data, { actor, item, missing="0", p
   return newFormula;
 }
 
+/**
+ * Create a unique id for a status condition.
+ * @param {string} status     The primary status.
+ * @returns {string}          A unique 16-character id.
+ */
+export function staticStatusId( status ) {
+  if ( status.length >= 16 ) return status.substring( 0, 16 );
+  return status.padEnd( 16, "0" );
+}
+
 /* -------------------------------------------- */
 /*  View Helper                                 */
 /* -------------------------------------------- */
