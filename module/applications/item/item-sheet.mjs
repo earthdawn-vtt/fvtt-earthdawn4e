288e60681aa00ea66922ab337b6d10e69fac0a1a
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-04-21 22:13:43 +0200

    remove class on top level, only needed on tabs

--- module/applications/item/item-sheet.mjs
+++ module/applications/item/item-sheet.mjs
@@ -17,7 +17,7 @@ export default class ItemSheetEd extends DocumentSheetMixinEd( ItemSheetV2 ) {
   static DEFAULT_OPTIONS = {
     id:       "item-sheet-{id}",
     uniqueId: String( ++foundry.applications.api.ApplicationV2._appId ),
-    classes:  [ "item", "scrollable" ],
+    classes:  [ "item" ],
     actions:  {
       config:             ItemSheetEd._onConfig,
     },
