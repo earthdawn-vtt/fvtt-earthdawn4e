ec867fb84c261ba16a154928d2e69852c2dc11a0
Chris S. D. <christopherDick00@gmail.com>
2025-04-21 18:59:35 +0200

    Add AttuneMatrixPrompt class

---
+++ module/applications/workflow/_module.mjs
@@ -0,0 +1,1 @@
+export { default as AttuneMatrixPrompt } from "./attune-matrix-prompt.mjs";
---
+++ module/applications/workflow/attune-matrix-prompt.mjs
@@ -0,0 +1,7 @@
+import ApplicationEd from "../api/application.mjs";
+
+export default class AttuneMatrixPrompt extends ApplicationEd {
+
+  static DEFAULT_OPTIONS = {};
+
+}
