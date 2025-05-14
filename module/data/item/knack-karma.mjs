0a9216142ea69b945b8f07ba9304c072d203584e
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-05-14 22:44:11 +0200

    fix mixin issue for knacks drop

--- module/data/item/knack-karma.mjs
+++ module/data/item/knack-karma.mjs
@@ -1,16 +1,12 @@
 import ItemDescriptionTemplate from "./templates/item-description.mjs";
 import KnackTemplate from "./templates/knack-item.mjs";
 import { ItemDataModel } from "../abstract.mjs";
-import { PromptFactory } from "../../applications/global/_module.mjs";
-import ED4E from "../../config/_module.mjs";
-import LearnableTemplate from "./templates/learnable.mjs";
 
 /**
  * Data model template with information on items that are used to represent custom active effects.
  */
 export default class KnackKarmaData extends ItemDataModel.mixin(
   KnackTemplate,
-  LearnableTemplate,
   ItemDescriptionTemplate
 ) {
 
@@ -25,145 +21,6 @@ export default class KnackKarmaData extends ItemDataModel.mixin(
     return this.mergeSchema( super.defineSchema(), {
             
     } );
-  }
-
-  /* -------------------------------------------- */
-  /*  LP Tracking                                 */
-  /* -------------------------------------------- */
-
-  /** @inheritDoc */
-  get canBeLearned() {
-    return true;
-  }
-
-  /**
-   * @inheritDoc
-   */
-  get learnData() {
-    const actor = this.parent._actor;
-
-    return {
-      talent:     actor.itemTypes.talent.find( ( talent ) => talent.system.edid === this.sourceTalent ),
-      requiredLp: this.requiredLpForLearning,
-      hasDamage:  actor.hasDamage( "standard" ),
-      hasWounds:  actor.hasWounds( "standard" ),
-      actor:      actor,
-    };
-  }
-
-  /**
-   * @inheritDoc
-   */
-  get requiredLpForLearning() {
-    // if no fixed lp cost is configured, use the default cost of a novice talent of the same rank as the min level.
-    if ( !this.lpCost ) {
-      return ED4E.legendPointsCost[
-        this.minLevel
-      ];
-    } else {
-      return this.lpCost;
-    }
-  }
-
-  /**
-   * @inheritDoc
-   */
-  get requiredMoneyForLearning() {
-    return ( this.minLevel ) * 50;
-  }
-
-  /** @inheritdoc */
-  get learnValidationData () {
-
-    const learnData = this.learnData;
-    return {
-      [ED4E.validationCategories.talentsRequirement]: [
-        {
-          name:      "ED.Dialogs.Legend.Validation.sourceTalentname",
-          value:     learnData.talent.name,
-          fulfilled: learnData.talent.isEmbedded
-        },
-        {
-          name:      "ED.Dialogs.Legend.Validation.sourceTalentRank",
-          value:     this.minLevel,
-          fulfilled: learnData.talent.system.level >= this.minLevel
-        },
-      ],
-      [ED4E.validationCategories.resources]: [
-        {
-          name:      "ED.Dialogs.Legend.Validation.availableLp",
-          value:     this.requiredLpForLearning,
-          fulfilled: this.requiredLpForLearning <= learnData.actor.currentLp,
-        },
-        {
-          name:      "ED.Dialogs.Legend.Validation.availableMoney",
-          value:     this.requiredMoneyForLearning,
-          fulfilled: this.requiredMoneyForLearning <= learnData.actor.currentSilver,
-        },
-      ],
-      [ED4E.validationCategories.health]:    [
-        {
-          name:      "ED.Dialogs.Legend.Validation.hasDamage",
-          value:     learnData.hasDamage,
-          fulfilled: !learnData.hasDamage,
-        },
-        {
-          name:      "ED.Dialogs.Legend.Validation.hasWounds",
-          value:     learnData.hasWounds,
-          fulfilled: !learnData.hasWounds,
-        },
-      ],
-    };
-  }
-
-  /** @inheritDoc */
-  static async learn( actor, item, createData = {} ) {
-    if ( !item.system.canBeLearned ) {
-      ui.notifications.warn( game.i18n.localize( "ED.Notifications.Warn.cannotLearn" ) );
-      return;
-    }
-
-    const parentTalent = actor.itemTypes.talent.find( ( talent ) => talent.system.edid === item.system.sourceTalent );
-    if ( !parentTalent ) {
-      const talentSourceEdid = item.system.sourceTalent;
-      ui.notifications.warn( game.i18n.format(
-        "ED.Notifications.Warn.noSourceTalent",
-        { talentSourceEdid: talentSourceEdid },
-      ) );
-      return;
-    }
-
-    const learnData = item;
-    learnData._actor = actor;
-
-    let learn = null;
-
-    const promptFactoryItem = await PromptFactory.fromDocument( learnData );
-    learn = await promptFactoryItem.getPrompt( "learnKnack" );
-
-    if ( !learn || learn === "cancel" || learn === "close" ) return;
-
-    const learnedItem = await KnackTemplate.learn( actor, item, createData );
-
-    const updatedActor = await actor.addLpTransaction(
-      "spendings",
-      {
-        amount:      learn === "spendLp" ? item.system.requiredLpForLearning : 0,
-        description: game.i18n.format(
-          "ED.Actor.LpTracking.Spendings",
-        ),
-        entityType:  learnedItem.type,
-        name:       learnedItem.name,
-        itemUuid:   learnedItem.uuid,
-      },
-    );
-
-    if ( foundry.utils.isEmpty( updatedActor ) )
-      ui.notifications.warn(
-        game.i18n.localize( "ED.Notifications.Warn.addLpTransactionProblems" )
-      );
-
-    return learnedItem;
   }
 
   /* -------------------------------------------- */
