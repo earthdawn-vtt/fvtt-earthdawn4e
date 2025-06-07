62cbb4540db57f0aaec0de37a12429c4f6899fb1
Chris S. D. <christopherDick00@gmail.com>
2025-06-07 17:00:13 +0200

    Implement grimoire lifecycle methods for data preparation and management

--- module/data/item/templates/grimoire.mjs
+++ module/data/item/templates/grimoire.mjs
@@ -45,4 +45,65 @@ export default class GrimoireTemplate extends SystemDataModel {
 
   // endregion
 
+  // region Life Cycle Events
+
+  /**
+   * Prepares the grimoire data for creation or update.
+   * @param {object} data The data to prepare, see {@link _preCreate} and {@link _preUpdate}.
+   */
+  _prepareGrimoireData( data ) {
+    const edidGrimoire = getSetting( "edidGrimoire" );
+
+    if ( this._isBecomingGrimoire( data, edidGrimoire ) ) {
+      this._setDefaultGrimoireData( data );
+    } else if ( this._isLosingGrimoire( data, edidGrimoire ) ) {
+      this._clearGrimoireData( data );
+    }
+  }
+
+  /**
+   * Checks if the item is becoming a grimoire with creation or update.
+   * @param {object} data The data to check, see {@link _preCreate} and {@link _preUpdate}.
+   * @param {string} edidGrimoire The EDID that defines a grimoire.
+   * @returns {boolean} True if the item is becoming a matrix, false otherwise.
+   */
+  _isBecomingGrimoire( data, edidGrimoire ) {
+    return data.system?.edid === edidGrimoire && this.edid !== edidGrimoire;
+  }
+
+  /**
+   * Sets the default grimoire data for a new grimoire.
+   * @param {object} data The data to set, see {@link _preCreate} and {@link _preUpdate}.
+   */
+  _setDefaultGrimoireData( data ) {
+    data.system.grimoire ??= {
+      spells: new Set(),
+      owner:  null,
+    };
+  }
+
+  /**
+   * Checks if the item is losing its grimoire status with creation or update.
+   * @param {object} data The data to check, see {@link _preCreate} and {@link _preUpdate}.
+   * @param {string} edidGrimoire The EDID that defines a grimoire.
+   * @returns {boolean} True if the item is losing its grimoire status, false otherwise.
+   */
+  _isLosingGrimoire( data, edidGrimoire ) {
+    return (
+      this.edid === edidGrimoire
+      && typeof data.system?.edid === "string"
+      && data.system.edid !== edidGrimoire
+    );
+  }
+
+  /**
+   * Clears the grimoire data when the item is no longer a grimoire.
+   * @param {object} data The data to clear, see {@link _preCreate} and {@link _preUpdate}.
+   */
+  _clearGrimoireData( data ) {
+    data.system.grimoire = null;
+  }
+
+  // endregion
+
 }
