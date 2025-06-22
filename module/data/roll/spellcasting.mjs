f89254f7c6e0ca7e56ce640d5c7160d674e8ce7b
Chris S. D. <christopherDick00@gmail.com>
2025-06-22 20:27:10 +0200

    Add SpellcastingRollOptions class and update roll flavor template

---
+++ module/data/roll/spellcasting.mjs
@@ -0,0 +1,67 @@
+import EdRollOptions from "./common.mjs";
+import { createContentAnchor } from "../../utils.mjs";
+
+export default class SpellcastingRollOptions extends EdRollOptions {
+
+  /** @inheritdoc */
+  static LOCALIZATION_PREFIXES = [
+    ...super.LOCALIZATION_PREFIXES,
+    "ED.Data.Other.SpellcastingRollOptions",
+  ];
+
+  /** @inheritdoc */
+  static TEST_TYPE = "action";
+
+  /** @inheritdoc */
+  static ROLL_TYPE = "spellcasting";
+
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    return this.mergeSchema( super.defineSchema(), {
+      spell: new fields.DocumentUUIDField( {
+        required: true,
+        type:     "Item",
+      } ),
+      spellcastingAbility: new fields.DocumentUUIDField( {
+        required: true,
+        type:     "Item",
+        embedded: true,
+      } ),
+    } );
+  }
+
+  /** @inheritDoc */
+  _getChatFlavorData() {
+    return {
+      sourceActor:         createContentAnchor( fromUuidSync( this.rollingActorUuid ) ).outerHTML,
+      spell:               createContentAnchor( fromUuidSync( this.spell ) ).outerHTML,
+      spellcastingAbility: createContentAnchor( fromUuidSync( this.spellcastingAbility ) ).outerHTML,
+    };
+  }
+
+  /** @inheritDoc */
+  _prepareStepData( data ) {
+    const ability = fromUuidSync( data.spellcastingAbility );
+    return {
+      base:      ability.system.rankFinal,
+      modifiers: {},
+    };
+  }
+
+  /** @inheritDoc */
+  _prepareStrainData( data ) {
+    const ability = fromUuidSync( data.spellcastingAbility );
+    return {
+      base: ability.system.strain,
+    };
+  }
+
+  /** @inheritDoc */
+  _prepareTargetDifficulty( data ) {
+    const spell = fromUuidSync( data.spell );
+    return {
+      base: spell.system.getDifficulty(),
+    };
+  }
+
+}
