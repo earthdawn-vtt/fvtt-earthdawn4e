bdba892c123a8ee1fd5faea55c524da2714f5727
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-07-14 23:46:29 +0200

    move level migration

---
+++ module/services/migrations/v082/field-migrations/level.mjs
@@ -0,0 +1,14 @@
+export default class LevelMigration {
+
+  static async migrateEarthdawnData( source ) {
+  
+    if ( source.system && !source.system.level && !source.system.ranks ) {
+      // No level or ranks defined, nothing to migrate
+      return;
+    }
+    // Migrate level from ranks if needed
+    if ( source.system ) {
+      source.system.level ??= source.system.ranks;
+    }
+  }
+}
