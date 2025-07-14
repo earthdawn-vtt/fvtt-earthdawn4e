aacc16b1f9e7e1f6c4b5de4dfeae2a64469f21b6
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-07-14 23:45:57 +0200

    move difficulty migration

---
+++ module/services/migrations/v082/field-migrations/difficulty.mjs
@@ -0,0 +1,21 @@
+import ED4E from "../../../../config/_module.mjs";
+
+export default class DifficultyMigration {
+
+  static async migrateEarthdawnData( source ) {
+
+    if ( source.system && !source.system.difficulty ) {
+      source.system.difficulty ??= {};
+      if ( source.system.difficulty.target === "" ) {
+        source.system.difficulty.target = "";
+      } else {
+        source.system.difficulty.target ??= Object.keys( ED4E.targetDifficulty )[ED4E.systemV0_8_2.targetDefense.indexOf( source.system.defenseTarget )];
+      }
+      if ( source.system.difficulty.group === "" ) {
+        source.system.difficulty.group = "";
+      } else {
+        source.system.difficulty.group ??= Object.keys( ED4E.groupDifficulty )[ED4E.systemV0_8_2.groupDifficulty.indexOf( source.system.defenseGroup )];
+      }
+    }
+  }
+}
