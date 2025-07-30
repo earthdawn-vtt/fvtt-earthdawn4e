63bf63c2ed97ac2f697ff5915fec6dad446748a4
Chris S. D. <christopherDick00@gmail.com>
2025-07-30 22:26:10 +0200

    Fix writing for data actions

--- module/data/chat/attack.mjs
+++ module/data/chat/attack.mjs
@@ -11,7 +11,7 @@ export default class AttackMessageData extends BaseMessageData {
   static DEFAULT_OPTIONS = {
     actions: {
       "applyEffect":  this._onApplyEffect,
-      "roll-damage":   this._onRollDamage,
+      "rollDamage":   this._onRollDamage,
       "maneuver":      this._onUseManeuver,
       "reaction":      this._onUseReaction,
     },
