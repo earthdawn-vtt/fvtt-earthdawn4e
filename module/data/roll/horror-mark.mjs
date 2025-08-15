eafec3ed9cda2710215c1067f3868c2f57860e80
Chris S. D. <christopherDick00@gmail.com>
2025-08-15 23:44:35 +0200

    Add documentation and move data initialization

--- module/data/roll/horror-mark.mjs
+++ module/data/roll/horror-mark.mjs
@@ -3,7 +3,29 @@ import { createContentAnchor } from "../../utils.mjs";
 import { MAGIC } from "../../config/_module.mjs";
 import { getSetting } from "../../settings.mjs";
 
+/**
+ * @typedef { object } EdHorrorMarkRollOptionsInitializationData
+ * @augments { EdRollOptionsInitializationData }
+ * @property { ActorEd } caster The actor that is casting the horror mark.
+ * Can be omitted if `casterUuid` in {@link HorrorMarkRollOptions} is provided.
+ * @property { ActorEd } [horror] The horror that is trying to mark the target.
+ * Can be omitted if `horrorUuid` in {@link HorrorMarkRollOptions} is provided.
+ * @property { ItemEd } [spell] The spell that is causing the horror mark,
+ * Can be omitted if `spellUuid` in {@link HorrorMarkRollOptions} is provided
+ */
+
+/**
+ * Roll options for horror mark rolls.
+ * @augments { EdRollOptions }
+ * @property { string } casterUuid The UUID of the actor that is casting the horror mark.
+ * @property { string } [astralSpacePollution] The astral space pollution level. Should be one of the keys
+ * in {@link module:config~MAGIC~astralSpacePollution}.
+ * @property { string } [horrorUuid] The UUID of the horror that is trying to mark the target.
+ * @property { string } [spellUuid] The spell that is causing the horror mark, if applicable.
+ */
 export default class HorrorMarkRollOptions extends EdRollOptions {
+
+  // region Static Properties
 
   /** @inheritdoc */
   static LOCALIZATION_PREFIXES = [
@@ -16,6 +38,10 @@ export default class HorrorMarkRollOptions extends EdRollOptions {
 
   /** @inheritdoc */
   static ROLL_TYPE = "horrorMark";
+
+  // endregion
+
+  // region Static Methods
 
   static defineSchema() {
     const fields = foundry.data.fields;
@@ -39,6 +65,28 @@ export default class HorrorMarkRollOptions extends EdRollOptions {
     } );
   }
 
+  /**
+   * @inheritDoc
+   * @param { EdHorrorMarkRollOptionsInitializationData & Partial<HorrorMarkRollOptions> } data The data to initialize the roll options with.
+   * @returns { HorrorMarkRollOptions } A new instance of HorrorMarkRollOptions.
+   */
+  static fromActor( data, actor, options = {} ) {
+    return /** @type { HorrorMarkRollOptions } */ super.fromActor( data, actor, options );
+  }
+
+  /**
+   * @inheritDoc
+   * @param { EdHorrorMarkRollOptionsInitializationData & Partial<HorrorMarkRollOptions> } data The data to initialize the roll options with.
+   * @returns { HorrorMarkRollOptions } A new instance of HorrorMarkRollOptions.
+   */
+  static fromData( data, options = {} ) {
+    return /** @type { HorrorMarkRollOptions } */ super.fromData( data, options );
+  }
+
+  // endregion
+
+  // region Data Initialization
+
   /** @inheritDoc */
   _getChatFlavorData() {
     const horror = this.horrorUuid ?
@@ -51,7 +99,7 @@ export default class HorrorMarkRollOptions extends EdRollOptions {
   }
 
   /** @inheritDoc */
-  _prepareStepData( data ) {
+  static _prepareStepData( data ) {
     if ( data.step ) return data.step;
 
     const horror = data.horror ?? fromUuidSync( data.horrorUuid );
@@ -84,17 +132,21 @@ export default class HorrorMarkRollOptions extends EdRollOptions {
   }
 
   /** @inheritDoc */
-  _prepareStrainData( data ) {
+  static _prepareStrainData( data ) {
     return null;
   }
 
   /** @inheritDoc */
-  _prepareTargetDifficulty( data ) {
+  static _prepareTargetDifficulty( data ) {
     const actor = data.caster ?? fromUuidSync( data.casterUuid );
     return {
       base: actor.system.characteristics.defenses.mystical.baseValue,
     };
   }
+
+  // endregion
+
+  // region Rendering
 
   /** @inheritDoc */
   async getFlavorTemplateData( context ) {
@@ -105,4 +157,6 @@ export default class HorrorMarkRollOptions extends EdRollOptions {
 
     return newContext;
   }
+
+  // endregion
 }
