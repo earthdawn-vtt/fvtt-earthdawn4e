b125fb77a50db12d68f931f19626ff3f6d9c1583
patrickmohrmann <patrick.mohrmann@outlook.de>
2025-09-04 11:20:23 +0200

    add circle as option to lelevl migration

--- module/services/migrations/v082/field-migrations/level.mjs
+++ module/services/migrations/v082/field-migrations/level.mjs
@@ -4,13 +4,18 @@ export default class LevelMigration extends BaseMigration {
 
   static async migrateEarthdawnData( source ) {
   
-    if ( source.system && !source.system.level && !source.system.ranks ) {
+    if ( source.system && !source.system.level && !source.system.ranks && !source.system.circle ) {
       // No level or ranks defined, nothing to migrate
       return;
     }
+    
     // Migrate level from ranks if needed
     if ( source.system ) {
-      source.system.level ??= source.system.ranks;
+      if ( source.type === "spell" ) {
+        source.system.level ??= source.system.circle;
+      } else {
+        source.system.level ??= source.system.ranks;
+      }
     }
   }
 }
