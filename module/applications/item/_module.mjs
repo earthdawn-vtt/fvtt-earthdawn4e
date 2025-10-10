1c84ed8656cda586e9732c6d07a04c3a9b8424da
Chris S. D. <christopherDick00@gmail.com>
2025-10-10 18:13:16 +0200

    Add ThreadItemSheet for rendering thread item details

--- module/applications/item/_module.mjs
+++ module/applications/item/_module.mjs
@@ -2,3 +2,4 @@ export { default as ItemSheetEd } from "./item-sheet.mjs";
 export { default as ClassItemSheetEd } from "./class-item-sheet.mjs";
 export { default as PhysicalItemSheetEd } from "./physical-item-sheet.mjs";
 export { default as MaskItemSheetEd } from "./mask-item-sheet.mjs";
+export { default as ThreadItemSheetEd } from "./thread-item-sheet.mjs";
