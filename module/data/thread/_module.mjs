05a709174da902acf3e2d3a6ae011c2deadca197
Chris S. D. <christopherDick00@gmail.com>
2025-10-10 17:33:51 +0200

    Add ThreadData model and integrate into module exports

--- module/data/thread/_module.mjs
+++ module/data/thread/_module.mjs
@@ -1,3 +1,2 @@
 export { default as TruePatternData } from "./true-pattern.mjs";
 export { default as ThreadItemLevelData } from "./thread-item-level.mjs";
-export { default as ThreadData } from "./thread.mjs";
