149ff66ee09dd7aa09f76993d3368152b103e326
Chris S. D. <christopherDick00@gmail.com>
2025-11-01 23:43:54 +0100

    Remove outdated comments from requiredLpForIncrease getter in true-pattern.mjs

--- module/data/thread/true-pattern.mjs
+++ module/data/thread/true-pattern.mjs
@@ -202,12 +202,6 @@ export default class TruePatternData extends SparseDataModel {
    * @type {number|undefined}
    */
   get requiredLpForIncrease() {
-    /* PG p. 229: "The Legend Point cost of Thread Ranks woven to the True
-        Patterns of people and places is the same as for improving Ranks of
-        a Novice talent" */
-    /* PG p. 224: "The Legend Point cost of a thread item usually follows the
-        progressions used to increase talent Ranks, but exactly which
-        progression (Novice, Journeyman, Warden, or Master) depends on the item." */
     const level = this.numberOfLevels + 1;
     const tierModifier = LEGEND.lpIndexModForTier[ 1 ][ this.tier ?? "novice" ];
     return LEGEND.legendPointsCost[ level + tierModifier ];
