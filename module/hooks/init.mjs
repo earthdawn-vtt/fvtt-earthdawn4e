655fa1b0e7e04c3730adade64f8c24a1896c359f
Chris S. D. <christopherDick00@gmail.com>
2025-11-16 17:31:53 +0100

    Refactor initialization logic into dedicated functions

--- module/hooks/init.mjs
+++ module/hooks/init.mjs
@@ -79,12 +79,153 @@ function setupDataModels() {
 /**
  *
  */
+function setupQueries() {
+  Object.assign( CONFIG.queries, ED4E.queries );
+}
+
+/**
+ *
+ */
+function setupRolls() {
+  CONFIG.Dice.rolls.splice( 0, 0, dice.EdRoll );
+}
+
+/**
+ *
+ */
+function setupTextEditor() {
+  enrichers.registerCustomEnrichers();
+}
+
+/**
+ *
+ */
+function registerSheetApps() {
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEd, {
+    makeDefault: true
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdCharacter, {
+    types:       [ "character" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdCharacter"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdNpc, {
+    types:       [ "npc" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdNpc"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdGroup, {
+    types:       [ "group" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdGroup"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdVehicle, {
+    types:       [ "vehicle" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdVehicle"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdLoot, {
+    types:       [ "loot" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdLoot"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdTrap, {
+    types:       [ "trap" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdTrap"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdCreature, {
+    types:       [ "creature" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdCreature"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdSpirit, {
+    types:       [ "spirit" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdSpirit"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdDragon, {
+    types:       [ "dragon" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdDragon"
+  } );
+  Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdHorror, {
+    types:       [ "horror" ],
+    makeDefault: true,
+    label:       "ED.Documents.actorSheetEdHorror"
+  } );
+
+  DocumentSheetConfig.unregisterSheet( ActiveEffect, "core", ActiveEffectConfig );
+  DocumentSheetConfig.registerSheet(
+    ActiveEffect,
+    "earthdawn4e",
+    applications.effect.EarthdawnActiveEffectSheet,
+    {
+      makeDefault: true,
+      label:       "ED.Documents.activeEffectSheetEd"
+    }
+  );
+
+  DocumentSheetConfig.unregisterSheet( Combatant, "core", CombatantConfig );
+  DocumentSheetConfig.registerSheet(
+    Combatant,
+    "earthdawn4e",
+    applications.combat.CombatantConfigEd,
+    { makeDefault: true }
+  );
+
+  Items.registerSheet( "earthdawn4e", applications.item.ItemSheetEd, {
+    makeDefault: true,
+    label:       "ED.Documents.itemSheetEd"
+  } );
+  Items.registerSheet( "earthdawn4e", applications.item.ClassItemSheetEd, {
+    types:       [ "discipline", "questor", "path" ],
+    makeDefault: true,
+    label:       "ED.Documents.itemSheetEdClass"
+  } );
+  Items.registerSheet( "earthdawn4e", applications.item.PhysicalItemSheetEd, {
+    types:       [ "armor", "equipment", "shield", "weapon" ],
+    makeDefault: true,
+    label:       "ED.Documents.itemSheetEdPhysical"
+  } );
+  Items.registerSheet( "earthdawn4e", applications.item.MaskItemSheetEd, {
+    types:       [ "mask" ],
+    makeDefault: true,
+    label:       "ED.Documents.itemSheetEdMask"
+  } );
+  Items.registerSheet( "earthdawn4e", applications.item.ThreadItemSheetEd, {
+    types:       [ "thread" ],
+    makeDefault: true,
+    label:       "ED.Documents.itemSheetEdThread"
+  } );
+
+  Journal.registerSheet( "earthdawn4e", applications.journal.JournalSheetEd, {
+    makeDefault: true,
+    label:       "ED.Documents.journalSheetEd"
+  } );
+
+  Scenes.unregisterSheet( "core", foundry.applications.sheets.SceneConfig );
+  Scenes.registerSheet( "earthdawn4e", applications.scene.SceneConfigEd, {
+    makeDefault: true,
+    label:       "ED.Documents.sceneConfigEd"
+  } );
+}
+
+/**
+ *
+ */
+function setupHandlebars() {
+  registerHandlebarHelpers();
+  utils.preloadHandlebarsTemplates();
+}
+
+/**
+ *
+ */
 export default function () {
   Hooks.once( "init", () => {
     globalThis.ed4e = game.ed4e = Object.assign( game.system, globalThis.ed4e );
     console.log( "ED4e | Initializing the ED4e Game System" );
-
-    // region Record Configuration Values
 
     CONFIG.ED4E = ED4E;
 
@@ -92,148 +233,14 @@ export default function () {
     setupCanvas();
     setupUI();
     setupCollections();
-
-    // Set up Queries
-    Object.assign( CONFIG.queries, ED4E.queries );
-
-    // Register Roll Extensions
-    CONFIG.Dice.rolls.splice( 0, 0, dice.EdRoll );
-
-    // Register text editor enrichers
-    enrichers.registerCustomEnrichers();
-
+    setupQueries();
+    setupRolls();
+    setupTextEditor();
     setupStatusEffects();
-
     setupDataModels();
-
-    // endregion
-
-    // Initialize migration system
     initializeMigrations();
-
-    // region Register Sheet Application Classes
-
-    // Actors.unregisterSheet( "core", foundry.appv1.sheets.ActorSheet );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEd, {
-      makeDefault: true
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdCharacter, {
-      types:       [ "character" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdCharacter"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdNpc, {
-      types:       [ "npc" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdNpc"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdGroup, {
-      types:       [ "group" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdGroup"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdVehicle, {
-      types:       [ "vehicle" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdVehicle"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdLoot, {
-      types:       [ "loot" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdLoot"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdTrap, {
-      types:       [ "trap" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdTrap"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdCreature, {
-      types:       [ "creature" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdCreature"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdSpirit, {
-      types:       [ "spirit" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdSpirit"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdDragon, {
-      types:       [ "dragon" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdDragon"
-    } );
-    Actors.registerSheet( "earthdawn4e", applications.actor.ActorSheetEdHorror, {
-      types:       [ "horror" ],
-      makeDefault: true,
-      label:       "ED.Documents.actorSheetEdHorror"
-    } );
-
-    DocumentSheetConfig.unregisterSheet( ActiveEffect, "core", ActiveEffectConfig );
-    DocumentSheetConfig.registerSheet(
-      ActiveEffect,
-      "earthdawn4e",
-      applications.effect.EarthdawnActiveEffectSheet,
-      { makeDefault: true, 
-        label:       "ED.Documents.activeEffectSheetEd",
-      },
-    );
-
-    DocumentSheetConfig.unregisterSheet( Combatant, "core", CombatantConfig );
-    DocumentSheetConfig.registerSheet(
-      Combatant,
-      "earthdawn4e",
-      applications.combat.CombatantConfigEd,
-      { makeDefault: true },
-    );
-
-    // Items.unregisterSheet( "core", foundry.appv1.sheets.ItemSheet );
-    Items.registerSheet( "earthdawn4e", applications.item.ItemSheetEd, {
-      makeDefault: true,
-      label:       "ED.Documents.itemSheetEd"
-    } );
-    Items.registerSheet( "earthdawn4e", applications.item.ClassItemSheetEd, { 
-      types:       [ "discipline", "questor", "path" ],
-      makeDefault: true,
-      label:       "ED.Documents.itemSheetEdClass" 
-    } );
-    Items.registerSheet( "earthdawn4e", applications.item.PhysicalItemSheetEd, { 
-      types:       [ "armor", "equipment", "shield", "weapon" ],
-      makeDefault: true,
-      label:       "ED.Documents.itemSheetEdPhysical"
-    } );
-    Items.registerSheet( "earthdawn4e", applications.item.MaskItemSheetEd, { 
-      types:       [ "mask" ],
-      makeDefault: true,
-      label:       "ED.Documents.itemSheetEdMask"
-    } );
-    Items.registerSheet( "earthdawn4e", applications.item.ThreadItemSheetEd, {
-      types:       [ "thread" ],
-      makeDefault: true,
-      label:       "ED.Documents.itemSheetEdThread"
-    } );
-
-    // Journal.unregisterSheet( "core", foundry.appv1.sheets.JournalSheet );
-    Journal.registerSheet( "earthdawn4e", applications.journal.JournalSheetEd, {
-      makeDefault: true,
-      label:       "ED.Documents.journalSheetEd"
-    } );
-
-    Scenes.unregisterSheet( "core", foundry.applications.sheets.SceneConfig );
-    Scenes.registerSheet( "earthdawn4e", applications.scene.SceneConfigEd, {
-      makeDefault: true,
-      label:       "ED.Documents.sceneConfigEd"
-    } );
-
-    // endregion
-
-    // region Handlebars
-
-    // Register Handlebars Helper
-    registerHandlebarHelpers();
-    // Preload Handlebars partials.
-    utils.preloadHandlebarsTemplates();
-
-    // endregion
+    registerSheetApps();
+    setupHandlebars();
 
   } );
 }
