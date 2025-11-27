3a5c092a4da0be3abe1d012f2384e97634a1a0f6
Chris S. D. <christopherDick00@gmail.com>
2025-11-27 23:10:10 +0100

    Replace ForeignDocumentField with SiblingDocumentField for questorDevotionId

--- module/data/item/questor.mjs
+++ module/data/item/questor.mjs
@@ -1,11 +1,13 @@
 import ClassTemplate from "./templates/class.mjs";
 import ItemDescriptionTemplate from "./templates/item-description.mjs";
 import { createContentLink, getSingleGlobalItemByEdid } from "../../utils.mjs";
-import ED4E from "../../config/_module.mjs";
 import PromptFactory from "../../applications/global/prompt-factory.mjs";
 import LpSpendingTransactionData from "../advancement/lp-spending-transaction.mjs";
 import DialogEd from "../../applications/api/dialog.mjs";
+import * as LEGEND from "../../config/legend.mjs";
+import * as DOCUMENT_DATA from "../../config/document-data.mjs";
 import { SYSTEM_TYPES } from "../../constants/constants.mjs";
+import SiblingDocumentField from "../fields/sibling-document-field.mjs";
 
 const { isEmpty } = foundry.utils;
 
@@ -23,13 +25,12 @@ export default class QuestorData extends ClassTemplate.mixin(
 
   /** @inheritDoc */
   static defineSchema() {
-    const fields = foundry.data.fields;
     return this.mergeSchema( super.defineSchema(), {
-      questorDevotionId: new fields.ForeignDocumentField(
+      questorDevotionId: new SiblingDocumentField(
         foundry.documents.Item,
         {
-          idOnly:    true,
-          nullable:  true,
+          systemTypes: [ SYSTEM_TYPES.Item.devotion, ],
+          nullable:    true,
         }
       ),
     } );
@@ -77,7 +78,7 @@ export default class QuestorData extends ClassTemplate.mixin(
     if ( !this.isActorEmbedded ) return undefined;
 
     return {
-      [ED4E.validationCategories.base]:               [
+      [LEGEND.validationCategories.base]:               [
         {
           name:      "ED.Dialogs.Legend.Validation.availableLp",
           value:     this.requiredLpForIncrease,
@@ -89,7 +90,7 @@ export default class QuestorData extends ClassTemplate.mixin(
 
   get requiredLpForIncrease() {
     // Questor devotion is treated as a journeyman talent
-    return ED4E.legendPointsCost[ this.unmodifiedLevel + 1 + ED4E.lpIndexModForTier[1].journeyman ];
+    return LEGEND.legendPointsCost[ this.unmodifiedLevel + 1 + LEGEND.lpIndexModForTier[1].journeyman ];
   }
 
   /** @inheritDoc */
@@ -102,7 +103,7 @@ export default class QuestorData extends ClassTemplate.mixin(
     const edidQuestorDevotion = game.settings.get( "ed4e", "edidQuestorDevotion" );
     let questorDevotion = actor.items.get( item.system.questorDevotionId );
     questorDevotion ??= await getSingleGlobalItemByEdid( edidQuestorDevotion, SYSTEM_TYPES.Item.devotion );
-    questorDevotion ??= await Item.create( ED4E.documentData.Item.devotion.questor );
+    questorDevotion ??= await Item.create( DOCUMENT_DATA.documentData.Item.devotion.questor );
 
     await questorDevotion.update( {
       system: {
