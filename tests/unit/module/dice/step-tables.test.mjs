7b892e880cce004a2911e8dd3836c346cfb13eb4
Chris S. D. <christopherDick00@gmail.com>
2026-01-10 18:33:47 +0100

    test: enhance basic test example

--- tests/unit/module/dice/step-tables.test.mjs
+++ tests/unit/module/dice/step-tables.test.mjs
@@ -1,6 +1,24 @@
-import { expect, test } from "vitest";
+import { describe, expect, it, } from "vitest";
 import { get3eDice } from "../../../../module/dice/step-tables.mjs";
 
-test( "step 5 returns '1d8' in 3e", () => {
-  expect( get3eDice( 5 ) ).toBe( "1d8" );
+describe( "3rd edition step table", () => {
+
+  it.for( [
+    { step: 1, expected: "1d6 - 3" },
+    { step: 5, expected: "1d8" },
+    { step: 100, expected: "13d12 + 1d8 + 1d6" },
+  ] )( "returns correct dice for steps in the range of [1;100]: $step => $expected", ( { step, expected } ) => {
+    expect( get3eDice( step ) ).toBe( expected );
+  } );
+
+  it.each( [
+    -1,
+    0,
+    101,
+    150,
+  ] )( "throws an error for steps outside the range of [1;100]: %i", ( step ) => {
+    const errorMessageRegex = /(?=.*step)(?=.*1)(?=.*100)/i ;
+    expect( () => get3eDice( step ) ).toThrow( errorMessageRegex );
+  } );
+
 } );
