2f5ad19f6958eb8058df11832d2b319c6530ea0b
Chris S. D. <christopherDick00@gmail.com>
2026-03-31 16:21:24 +0200

    refactor: centralize formula methods in `formula-field`

--- module/data/fields/formula-field.mjs
+++ module/data/fields/formula-field.mjs
@@ -10,6 +10,8 @@
  */
 export default class FormulaField extends foundry.data.fields.StringField {
 
+  // region Static Getters
+
   /** @inheritDoc */
   static get _defaults() {
     return foundry.utils.mergeObject( super._defaults, {
@@ -17,41 +19,9 @@ export default class FormulaField extends foundry.data.fields.StringField {
     } );
   }
 
-  /* -------------------------------------------- */
-  /* Properties                                   */
-  /* -------------------------------------------- */
+  // endregion
 
-  get placeholderHint() {
-    return "e.g. 2*@rank + @extras";
-  }
-
-  /* -------------------------------------------- */
-  /* Cleaning & Validation                        */
-  /* -------------------------------------------- */
-
-  /** @inheritDoc */
-  _validateType( value ) {
-    if ( this.options.deterministic ) {
-      const roll = new Roll( value );
-      if ( !roll.isDeterministic ) throw new Error( "must not contain dice terms" );
-      Roll.safeEval( roll.formula );
-    } else Roll.validate( value );
-    super._validateType( value );
-  }
-
-  /* -------------------------------------------- */
-  /* Rendering                                    */
-  /* -------------------------------------------- */
-
-  /** @inheritDoc */
-  _toInput( config ) {
-    config.placeholder ??= this.placeholderHint;
-    return super._toInput( config );
-  }
-
-  /* -------------------------------------------- */
-  /* Evaluation                                   */
-  /* -------------------------------------------- */
+  // region Static Methods
 
   /**
    * Evaluate a formula using a provided data object
@@ -66,5 +36,100 @@ export default class FormulaField extends foundry.data.fields.StringField {
     );
   }
 
-}
+  /**
+   * Prepare the final formula value for a model field.
+   * @param {ItemDataModel} model  Model for which the value is being prepared.
+   * @param {string} keyPath                        Path to the field within the model.
+   * @param {string} label                          Label to use in preparation warnings.
+   * @param {object} rollData                       Roll data to use when replacing formula values.
+   */
+  static prepareFormulaValue( model, keyPath, label, rollData ) {
+    const value = foundry.utils.getProperty( model, keyPath );
+    if ( !value ) return;
+    const item = model.item ?? model.parent;
+    const property = game.i18n.localize( label );
+    try {
+      const formula = this.replaceFormulaData( value, rollData, { item, property } );
+      const roll = new Roll( formula );
+      foundry.utils.setProperty( model, keyPath, roll.evaluateSync().total );
+    } catch ( err ) {
+      if ( item.isEmbedded ) {
+        const message = game.i18n.format( "ED.Notifications.Error.formulaMalformedError", {
+          property,
+          name: model.name ?? item.name
+        } );
+        // item.actor._preparationWarnings.push( { message, link: item.uuid, type: "error" } );
+        console.error( message, err );
+      }
+    }
+  }
 
+  /**
+   * Replace referenced data attributes in the roll formula with values from the provided data.
+   * If the attribute is not found in the provided data, display a warning on the actor.
+   * @param {string} formula           The original formula within which to replace.
+   * @param {object} data              The data object that provides replacements.
+   * @param {object} [options]         Options for the replacement process.
+   * @param {ActorEd} [options.actor]            Actor for which the value is being prepared.
+   * @param {ItemEd} [options.item]              Item for which the value is being prepared.
+   * @param {string|null} [options.missing]  Value to use when replacing missing references, or `null` to not replace.
+   * @param {string} [options.property]          Name of the property to which this formula belongs.
+   * @returns {string}                 Formula with replaced data.
+   */
+  static replaceFormulaData( formula, data, { actor, item, missing = "0", property } = {} ) {
+    const dataRgx = new RegExp( /@([a-z.0-9_-]+)/gi );
+    const missingReferences = new Set();
+    const newFormula = String( formula ).replace( dataRgx, ( match, term ) => {
+      let value = foundry.utils.getProperty( data, term );
+      if ( value === null || value === undefined ) {
+        missingReferences.add( match );
+        return missing ?? match[0];
+      }
+      return String( value ).trim();
+    } );
+    const newActor = actor ?? item?.parent;
+    if ( ( missingReferences.size > 0 ) && newActor && property ) {
+      const listFormatter = new Intl.ListFormat( game.i18n.lang, { style: "long", type: "conjunction" } );
+      const message = game.i18n.format( "ED.Notifications.Error.formulaMissingReferenceWarn", {
+        property, name: item?.name ?? newActor.name, references: listFormatter.format( missingReferences )
+      } );
+      newActor._preparationWarnings.push( { message, link: item?.uuid ?? newActor.uuid, type: "warning" } );
+    }
+    return newFormula;
+  }
+
+  // endregion
+
+  // region Getters
+
+  get placeholderHint() {
+    return "e.g. 2*@rank + @extras";
+  }
+
+  // endregion
+
+  // region Validation
+
+  /** @inheritDoc */
+  _validateType( value ) {
+    if ( this.options.deterministic ) {
+      const roll = new Roll( value );
+      if ( !roll.isDeterministic ) throw new Error( "must not contain dice terms" );
+      Roll.safeEval( roll.formula );
+    } else Roll.validate( value );
+    super._validateType( value );
+  }
+
+  // endregion
+
+  // region Rendering
+
+  /** @inheritDoc */
+  _toInput( config ) {
+    config.placeholder ??= this.placeholderHint;
+    return super._toInput( config );
+  }
+
+  // endregion
+
+}
