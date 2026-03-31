132129683dddbbeb1a43155f9783333e133c121d
Chris S. D. <christopherDick00@gmail.com>
2026-03-31 16:51:03 +0200

    refactor: move utils to dedicated `utils/_module` structure

---
+++ module/utils/_module.mjs
@@ -0,0 +1,9 @@
+export * as earthdawn from "./earthdawn.mjs";
+export * as math from "./math.mjs";
+export * as object from "./object.mjs";
+export * as string from "./string.mjs";
+
+export * from "./earthdawn.mjs";
+export * from "./math.mjs";
+export * from "./object.mjs";
+export * from "./string.mjs";
