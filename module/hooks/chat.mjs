2148ea4f9469e330ca0c6921e7cf91698a309aca
Chris S. D. <christopherDick00@gmail.com>
2026-04-20 20:31:29 +0200

    fix: sanitize chat commands by stripping HTML tags

--- module/hooks/chat.mjs
+++ module/hooks/chat.mjs
@@ -31,19 +31,14 @@ export default function () {
    * /s Triggers Step roll
    */
   Hooks.on( "chatMessage", ( html, content, msg ) => {
-    if ( !Object.keys( CONFIG.ED4E.chatCommands ).some( ( cmd ) => content.startsWith( `/${cmd.toLowerCase()}` ) ) ) {
+    const strippedContent = content.replace( /^<p>|<\/p>$/gi, "" );
+    if ( !Object.keys( CONFIG.ED4E.chatCommands ).some( ( cmd ) => strippedContent.startsWith( `/${cmd.toLowerCase()}` ) ) ) {
       // No ED command, continue Foundry workflow
       return true;
     }
 
-    // Setup new message's visibility
-    let rollMode = game.settings.get( "core", "rollMode" );
-    if ( [ "gmroll", "blindroll" ].includes( rollMode ) )
-      msg["whisper"] = ChatMessage.getWhisperRecipients( "GM" ).map( ( u ) => u.id );
-    if ( rollMode === "blindroll" ) msg["blind"] = true;
-
     const cmdRegExp = /(?<command>\/\w+)(?<arguments>.*)/;
-    const commandMatches = content.match( cmdRegExp );
+    const commandMatches = strippedContent.match( cmdRegExp );
 
     return cmdMapping[commandMatches.groups.command.substring( 1 )]( commandMatches.groups.arguments.trim() );
   } );
