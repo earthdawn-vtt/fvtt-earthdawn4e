c2a487c54ffd4e366b3dc6f5cd89bd94a73825e7
Chris S. D. <christopherDick00@gmail.com>
2026-07-11 09:52:47 +0200

    docs: update Typedoc theme

--- module/constants/_module.mjs
+++ module/constants/_module.mjs
@@ -1,4 +1,11 @@
 /** @module constants */
 
 import * as ED4E_CONSTANTS from "./constants.mjs";
+
+export * from "./constants.mjs";
+
+/**
+ * @ignore
+ * @namespace ED4E_CONSTANTS
+ */
 export default ED4E_CONSTANTS;
