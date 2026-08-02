a4517e20e54dc4953de04b0e15da457a8ea43ba4
Chris S. D. <christopherDick00@gmail.com>
2026-08-02 11:38:25 +0200

    ci: add required doc for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import babelParser from "@babel/eslint-parser";
 import globals from "globals";
 import js from "@eslint/js";
+import jsdoc from "eslint-plugin-jsdoc";
 import typedocPlugin from "eslint-plugin-typedoc";
 import path from "node:path";
 import stylistic from "@stylistic/eslint-plugin";
@@ -23,6 +24,7 @@ export default [
     plugins: {
       "@stylistic":         stylistic,
       "@stylistic/migrate": stylisticMigrate,
+      "jsdoc":              jsdoc,
       "typedoc":            typedocPlugin,
     },
     files:           [ "**/*.mjs", "**/*.js", ],
@@ -395,6 +397,23 @@ export default [
       "@stylistic/space-in-parens": [ "error", "always" ],
       strict:                       [ "error", "never" ],
       "symbol-description":         "error",
+
+      "jsdoc/require-jsdoc": [
+        "error",
+        {
+          "checkGetters":             true,
+          "checkSetters":             "no-getter",
+          "enableFixer":              false,
+          "require":                  {
+            "ArrowFunctionExpression":  false,
+            "ClassDeclaration":         true,
+            "ClassExpression":          true,
+            "FunctionDeclaration":      true,
+            "FunctionExpression":       true,
+            "MethodDefinition":         true,
+          },
+        }
+      ],
 
       "typedoc/require-param-tag-description": "error",
       "typedoc/require-returns-description":   "error",
